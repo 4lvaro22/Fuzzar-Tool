89a60a048fb4eb2c0193277cb6d76395eb875c8c
Alvaro <pl.alvaro43@uma.es>
2024-05-14 16:31:18 +0200

    first version done 👌

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.locals.moment = moment;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- app.sh
+++ app.sh
@@ -17,90 +17,22 @@ reset_all_env_variables () {
     done
 }
 
-ask_build_system () {
-    while true; do
-        echo -e "\n$blue_color[+]$grey_color Selecciona el system build que usa la aplicación."
-        counter=1
-        for build_system in "${build_systems[@]}"; do
-            echo "$counter) $build_system"
-            counter=$((counter+1))
-        done
-        echo -ne "\n$yellow_color[?]$grey_color Selecciona una opción: " && read reply_build_system
-        if [ "$reply_build_system" -gt "$counter" ]; then
-            echo -e "\n$red_color[-]$reset_color Opcion no encontrada." 
-        else
-            break
-        fi
-    done
-}
-
-ask_sanitizer () {
-    while true; do
-        echo -e "\n$blue_color[+]$grey_color Selecciona la exploración de errores deseada."
-        counter=1
-        for sanitizer in "${analysis_desc[@]}"; do
-            echo "$counter) $sanitizer"
-            counter=$((counter+1))
-        done
-        echo -ne "\n$yellow_color[?]$grey_color Selecciona una opción: " && read reply_sanitizer
-        if [ "$reply_sanitizer" -gt "$counter" ] || [ "$reply_sanitizer" -lt "0" ]; then
-            echo -e "\n$red_color[-]$reset_color Opcion no encontrada." 
-        else
-            break
-        fi
-    done
-}
-
-
-afl() {
-    reset_all_env_variables
-    ask_sanitizer
-    export "${analysis_comm[reply_sanitizer-1]}=1"
-    echo -e "$blue_color[+]$grey_color Fuzzer escogido AFLPlusPlus."
-    echo -e "$blue_color[+]$grey_color Exploración de errores escogida "${analysis[reply_sanitizer]}"."
-    echo -e "$blue_color[+]$grey_color Tiempo estimado "${execution_time}" segundos."
-    echo -e "$blue_color[+]$grey_color Ejecutando análisis..."
-    current_date=$(date +"%d%m%Y-%H%M%S")
-    afl-fuzz -V $execution_time -i inputs/ -o "results/result-"$current_date"" -- $1 -t /dev/stdin > /dev/null 2>&1;
-    finilizing_rutine
-}
-
-honggfuzz() {
-    echo -e "$blue_color[+]$grey_color Fuzzer escogido HonggFuzz."
-    echo -e "$blue_color[+]$grey_color La exploración de errores para HonggFuzz es la estándar."
-    echo -e "$blue_color[+]$grey_color Tiempo estimado "${execution_time}" segundos."
-    echo -e "$blue_color[+]$grey_color Ejecutando análisis..."
-    current_date=$(date +"%d%m%Y-%H%M%S")
-    honggfuzz -t $execution_time -V -i inputs/ -o "results/result-"$current_date"" -s -- $1 -t /dev/stdin > /dev/null 2>&1;
-    finilizing_rutine
-}
-
-execute_fuzzer() { 
-    cd $original_path;
-    if [[ "$fuzzer" == "honggfuzz" ]]; then
-        honggfuzz $1
-    else
-        afl $1
-    fi
-}
+# afl() {
+#     reset_all_env_variables
+#     ask_sanitizer
+#     export "${analysis_comm[reply_sanitizer-1]}=1"
+#     echo -e "$blue_color[+]$grey_color Fuzzer escogido AFLPlusPlus."
+#     echo -e "$blue_color[+]$grey_color Exploración de errores escogida "${analysis[reply_sanitizer]}"."
+#     echo -e "$blue_color[+]$grey_color Tiempo estimado "${execution_time}" segundos."
+#     echo -e "$blue_color[+]$grey_color Ejecutando análisis..."
+#     current_date=$(date +"%d%m%Y-%H%M%S")
+#     afl-fuzz -V $execution_time -i inputs/ -o "results/result-"$current_date"" -- $1 -t /dev/stdin > /dev/null 2>&1;
+#     finilizing_rutine
+# }
 
 execute_web_app(){
     npm start -s &
     printf "\n$blue_color[+]$grey_color Tiene los datos listos para su visualización en $cyan_color\e]8;;http://localhost:3000\ahttp://localhost:3000\e]8;;\a$grey_color.\n"
-}
-
-finilizing_rutine() {
-    chmod -R 777 results/*
-    echo -e "$green_color[✓]$grey_color Todo ha funcionado correctamente. Tiene los resultados en el directorio output."
-    python3 script/data_modifier.py $original_path AFLPlusPlus $execution_time $current_date
-    
-    echo -ne "\n$yellow_color[?]$grey_color Quiere realizar otra prueba fuzzing? (s/N): "
-    read loop
-    if  [ "$loop" == "S" ] || [ "$loop" == "s" ]; then 
-        clear
-        cat tool_logo.txt
-        main
-    fi
 }
 
 help()
@@ -118,7 +50,7 @@ version()
 }
 
 initialize_variables(){
-    build_systems=("Configure build system" "CMake build system" "Meson build system")
+    data_source_list=("OBD-II" "CAN Bus" "No predeterminada")
     compilers=("AFLPlusPlus + afl-clang-lto/afl-clang-lto++" "AFLPlusPlus + afl-clang-fast/afl-clang-fast++" "AFLPlusPlus + afl-gcc-fast/afl-g++-fast" "AFLPlusPlus + afl-gcc/afl-g++" "AFLPlusPlus + afl-clang/afl-clang++")
     analysis_desc=("Estándar: sigue una exploración de errores estándar." "ASAN: direcciones de memoria." "MSAN: lecturas de memoria no inicializadas." "UBSAN: comportamientos no definidos." "CFISAN: flujos de control ilegales" "TSAN: hilos de ejecución vulnerables.")
     analysis_comm=("" "AFL_USE_ASAN" "AFL_USE_MSAN" "AFL_USE_UBSAN" "AFL_USE_CFISAN" "AFL_USE_TSAN")
@@ -130,19 +62,53 @@ initialize_variables(){
 main() {
     original_path=$(pwd)
 
-    python3 web_scrapper.py
+    while true; do
+        echo -e "\n$blue_color[+]$grey_color Selecciona si quieres una recolección de datos predeterminada."
+        counter=1
+        for el_source_list in "${data_source_list[@]}"; do
+            echo "$counter) $el_source_list"
+            counter=$((counter+1))
+        done
+        echo -ne "\n$yellow_color[?]$grey_color Selecciona una opción: " && read reply_data_source_list
+        
+        if [[ "$reply_build_system" -gt "$counter" ]]; then
+            echo -e "\n$red_color[-]$reset_color Opcion no encontrada." 
+        else
+            data_source=${data_source_list[$reply_data_source_list-1]}
+            break
+        fi
+    done
 
-    echo -ne "\n$yellow_color[?]$grey_color Introduce el directorio raíz donde se encuentra la aplicación: "
-    read path
+    echo -ne "\n$yellow_color[?]$grey_color Introduce el directorio de errores del análisis: "
+    read errors_directory
 
-    ask_build_system
-    compiling_simulator
+    echo -ne "$yellow_color[?]$grey_color Introduce el directorio raíz del simulador o fichero: "
+    read path_simulator
 
-    echo -ne "\n$yellow_color[?]$grey_color Introduce el directorio del archivo binario del simulador: "
-    read path_sim
+    echo -ne "$yellow_color[?]$grey_color Introduce la configuración del compilador deseada: "
+    read config_compilator
+
+    echo -ne "$yellow_color[?]$grey_color Introduce la configuración del fuzzing deseada: "
+    read config_fuzzing
+
+    echo -ne "$yellow_color[?]$grey_color Introduce la descripción deseada de la prueba: "
+    read description
+ 
+    name="Terminal"
     
-    execute_fuzzer "${path_sim}"
-}
+    echo -ne "\n$blue_color[+]$grey_color Ejecutando análisis..."
+    echo -ne "\n$blue_color[+]$grey_color Esto puede durar varios minutos!"
+    bash ./webapp.sh "$data_source" "$path_simulator" "$config_compilator" "$config_fuzzing" "$errors_directory" "$description" "$name"
+
+    echo -ne "\n$yellow_color[?]$grey_color Quiere realizar otra prueba fuzzing? (s/N): "
+    read loop
+    if  [ "$loop" == "S" ] || [ "$loop" == "s" ]; then 
+        clear
+        cat tool_logo.txt
+        main
+    fi
+
+}   
 
 cleanup() {
     pkill -P $$
@@ -156,6 +122,7 @@ if  [ $(id -u) -ne 0 ]; then
     exit -1
 fi
 
+execute_web_app
 initialize_variables
 
 while getopts "hvs --" option; do
@@ -183,13 +150,6 @@ if [ -f "data.json" ]
 then
    touch data.json;
    chmod 777 data.json;
-fi
-
-echo -ne "\n$yellow_color[?]$grey_color Quieres desplegar la visualización de datos mediante una aplicación web? (s/N): "
-read app_web
-
-if [ "$app_web" == "s" ] || [ "$app_web" == "S" ]; then 
-    execute_web_app
 fi
 
 main
